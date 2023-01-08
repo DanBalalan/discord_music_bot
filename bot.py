cfe14e308736465ce179e9c3f2a2f1ccb575a3e7
Daniil S <danbalalan@gmail.com>
2023-01-08 08:25:59 +0300

    bump reqs

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
+import asyncio
 import os
+
+from discord import Intents
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -8,11 +11,21 @@ load_dotenv('env.py', override=True)
 
 bot = commands.Bot(
     command_prefix=commands.when_mentioned_or(os.environ['COMMAND_PREFIX']),
-    description=None
+    description=None,
+    intents=Intents.all()
 )
 
-for cog in all_cogs:
-    bot.add_cog(cog(bot))
+# for cog in all_cogs:
+#     bot.add_cog(cog(bot))
+
+# bot.run(os.environ['BOT_KEY'])
+
+async def main():
+    async with bot:
+        for cog in all_cogs:
+            await bot.add_cog(cog(bot))
+
+        await bot.start(os.environ['BOT_KEY'])
 
 
-bot.run(os.environ['BOT_KEY'])
+asyncio.run(main())
